44d035f98d4b1d0a00b6d35fc9261edf16800a2b
harshavardhan7209 <66862844+harshavardhan7209@users.noreply.github.com>
2022-05-29 13:56:48 +0530

    Update app.java

--- app.java
+++ app.java
@@ -2,55 +2,65 @@ import java.util.*;
 public class app {
     public static void main(String [] args) {
         while (true) {
-            List<Long> m = new ArrayList<Long>();
-            List<String> mn = new ArrayList<String>();
-            Scanner x = new Scanner(System.in);
+            int y;
+            List<Integer> l = new ArrayList<Integer>();
+            Scanner sc = new Scanner(System.in);
             System.out.print("Enter the hypotenuse: \u221A");
-            Long y = x.nextLong();
-            List<Long> l = new ArrayList<Long>();
+            y = sc.nextInt();
             l.add(y);
-            l.add((long)0);
-            for (int i = 0; i < l.size(); i++) {
-                while (Math.round(Math.sqrt(l.get(i))) != Math.sqrt(l.get(i))) {
-                    List<String> no = new ArrayList<String>();
-                    m = numberline(l.get(i));
-                    for (int j = 0; j <m.size(); j++) {
-                        if (Math.round(Math.sqrt(m.get(j))) == Math.sqrt(m.get(j))) {
-                            no.add(Long.toString((long)Math.sqrt(m.get(j))));
-                        }else {
-                            no.add("\u221A" + m.get(j));
+            l.add((int)0);
+            if (Math.round(Math.sqrt(l.get(0))) != Math.sqrt(l.get(0))) {
+                for (int i = 0; i < l.size(); i++) {
+                    while (Math.round(Math.sqrt(l.get(i))) != Math.sqrt(l.get(i))) {
+                        List<Integer> m = new ArrayList<Integer>();
+                        List<String> no = new ArrayList<String>();
+                        m = numberline(l.get(i));
+                        System.out.println(m);
+                        for (int j = 0; j < m.size(); j++) {
+                            if (Math.round(Math.sqrt(m.get(j))) == Math.sqrt(m.get(j))) {
+                                no.add(Integer.toString((int)(Math.sqrt(m.get(j)))));
+                            } else {
+                                no.add("\u221A" + m.get(j));
+                            }
                         }
+                        System.out.println("The altitude and base respectively for \u221A" + l.get(i) + " are: [" + no.get(0) + "," + no.get(1) + "]");
+                        l = m;
                     }
-                    System.out.println("The altitude and base respectively for \u221A" + l.get(i).intValue() + " are: " + no);
-                    l = m;
                 }
+            }else {
+                System.out.println("The altitude and base respectively for \u221A" + y + " are: [" + (int)Math.sqrt(l.get(0)) + "," + l.get(1) + "]");
+
             }
+
         }
     }
-    public static List<Long> numberline(long a) {
-        long abcd = 0, efgh = 0, za, zc;
-        List<Long> e = new ArrayList<Long>();
-        List<ArrayList<Long>> h = new ArrayList<ArrayList<Long>>();
-        for (long i=0;i<(a+1);i++) {
-            for (long j=0;j<(i+1);j++) {
+    public static List<Integer> numberline(int a) {
+        int za, z;
+        List<Integer> e = new ArrayList<Integer>();
+        List<Integer> aeh = new ArrayList<Integer>();
+        List<ArrayList<Integer>> h = new ArrayList<ArrayList<Integer>>();
+        for (int i=0;i<=a;i++) {
+            for (int j=0;j<=i;j++) {
                 if (i+j==a) {
-                    long m,z;
-                    ArrayList<Long> zd = new ArrayList<Long>();
-                    if (Math.round(Math.sqrt(i)) == Math.sqrt(i)) {
-                        m = (long)(Math.sqrt(i));
-                    }else{
-                        m = i;
+                    ArrayList<Integer> zd = new ArrayList<Integer>();
+                    if ((int)(Math.sqrt(i)) == Math.sqrt(i) || (int)(Math.sqrt(j)) == Math.sqrt(j)) {
+                        int l,y;
+                        if ((int)(Math.sqrt(i)) == Math.sqrt(i)) {
+                            l = (int)(Math.sqrt(i));
+                        } else {
+                            l = i;
+                        }
+                        if (Math.round(Math.sqrt(j)) == Math.sqrt(j)) {
+                            y = (int)(Math.sqrt(j));
+                        } else {
+                            y = j;
+                        }
+                        z = (l+y)+0;
+                        zd.add(i);
+                        zd.add(j);
+                        e.add(z);
+                        h.add(zd);
                     }
-                    if (Math.round(Math.sqrt(j)) == Math.sqrt(j)) {
-                        z = (long)(Math.sqrt(j));
-                    }else{
-                        z = j;
-                    }
-                    zc = (m+z)*1;
-                    zd.add(i);
-                    zd.add(j);
-                    e.add(zc);
-                    h.add(zd);
                 }
             }
         }
@@ -60,10 +70,17 @@ public class app {
         }
         for(int i=0;i<h.size();i++) {
             if (za == e.get(i)) {
-                abcd = (h.get(i).get(0));
-                efgh = (h.get(i).get(1));
+                if (h.get(i).get(1) == 0) {
+                    if ((int)(Math.sqrt(h.get(i).get(0))) == Math.sqrt(h.get(i).get(0))) {
+                        aeh.add(h.get(i).get(0));
+                        aeh.add((int)0);
+                    }
+                } else {
+                    aeh.add(h.get(i).get(0));
+                    aeh.add(h.get(i).get(1));
+                }
             }
         }
-       return Arrays.asList(abcd,efgh);
+       return aeh;
     }
 }
