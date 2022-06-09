9b5b208edefc400b708dffc7f44db1a541bc0da6
harshavardhan7209 <66862844+harshavardhan7209@users.noreply.github.com>
2022-06-09 13:00:46 +0530

    Making the app more readable
    Added comments, giving better naming to variable, doing better code formatting

--- app.java
+++ app.java
@@ -2,85 +2,102 @@ import java.util.*;
 public class app {
     public static void main(String [] args) {
         while (true) {
-            int y;
-            List<Integer> l = new ArrayList<Integer>();
+            //Storing user input
+            int user_input;
+            //Defining list to store the perpendicular and hypotenuse
+            List<Integer> all_results = new ArrayList<Integer>();
+            //Syntax to get input from user
             Scanner sc = new Scanner(System.in);
             System.out.print("Enter the hypotenuse: \u221A");
-            y = sc.nextInt();
-            l.add(y);
-            l.add((int)0);
-            if (Math.round(Math.sqrt(l.get(0))) != Math.sqrt(l.get(0))) {
-                for (int i = 0; i < l.size(); i++) {
-                    while (Math.round(Math.sqrt(l.get(i))) != Math.sqrt(l.get(i))) {
-                        List<Integer> m = new ArrayList<Integer>();
+            user_input = sc.nextInt();
+            //Adding user input to be calculated
+            all_results.add(user_input);
+            all_results.add((int)0);
+            //Calculating perpendicular and hypotenuse for the given user input
+            if (Math.round(Math.sqrt(all_results.get(0))) != Math.sqrt(all_results.get(0))) {//If the user input is a perfect square
+                for (int i = 0; i < all_results.size(); i++) {
+                    while (Math.round(Math.sqrt(all_results.get(i))) != Math.sqrt(all_results.get(i))) {// Looping until the user input is a perfect square
+                        List<Integer> result = new ArrayList<Integer>();
                         List<String> no = new ArrayList<String>();
-                        m = numberline(l.get(i));
-                        System.out.println(m);
-                        for (int j = 0; j < m.size(); j++) {
-                            if (Math.round(Math.sqrt(m.get(j))) == Math.sqrt(m.get(j))) {
-                                no.add(Integer.toString((int)(Math.sqrt(m.get(j)))));
+                        result = numberline(all_results.get(i));
+                        System.out.println(result);
+                        //Checking is the result perpendicular and hypotenuse are perfect squares
+                        for (int j = 0; j < result.size(); j++) {
+                            if (Math.round(Math.sqrt(result.get(j))) == Math.sqrt(result.get(j))) {
+                                no.add(Integer.toString((int)(Math.sqrt(result.get(j)))));
                             } else {
-                                no.add("\u221A" + m.get(j));
+                                no.add("\u221A" + result.get(j));
                             }
                         }
-                        System.out.println("The altitude and base respectively for \u221A" + l.get(i) + " are: [" + no.get(0) + "," + no.get(1) + "]");
-                        l = m;
+                        //Printing to the user the results
+                        System.out.println("The altitude and base respectively for \u221A" + all_results.get(i) + " are: [" + no.get(0) + "," + no.get(1) + "]");
+                        all_results = result;
                     }
                 }
             }else {
-                System.out.println("The altitude and base respectively for \u221A" + y + " are: [" + (int)Math.sqrt(l.get(0)) + "," + l.get(1) + "]");
+                System.out.println("The altitude and base respectively for \u221A" + user_input + " are: [" + (int)Math.sqrt(all_results.get(0)) + "," + all_results.get(1) + "]");
 
             }
 
         }
     }
+    //The function that calculates hypotenuse and perpendicular of a particular number
     public static List<Integer> numberline(int a) {
-        int za, z;
-        List<Integer> e = new ArrayList<Integer>();
-        List<Integer> aeh = new ArrayList<Integer>();
-        List<ArrayList<Integer>> h = new ArrayList<ArrayList<Integer>>();
+        //Defining variables and required lists
+        int smart_sum, sum;
+        // Stores all the possible values of hypotenuse and perpendicular
+        List<Integer> all_sums = new ArrayList<Integer>();
+        //Stores the recommended values of hypotenuse and perpendicular
+        List<Integer> returner = new ArrayList<Integer>();
+        List<ArrayList<Integer>> all_perpendicular_and_hypotenuse = new ArrayList<ArrayList<Integer>>();
         for (int i=0;i<=a;i++) {
             for (int j=0;j<=i;j++) {
                 if (i+j==a) {
-                    ArrayList<Integer> zd = new ArrayList<Integer>();
+                    ArrayList<Integer> perpendicular_and_hypotenuse = new ArrayList<Integer>();
                     if ((int)(Math.sqrt(i)) == Math.sqrt(i) || (int)(Math.sqrt(j)) == Math.sqrt(j)) {
-                        int l,y;
+                        //Defining variables
+                        int perpendicular,hypotenuse;
+                        // Calculating and storing values of perpendicular and hypotenuse
                         if ((int)(Math.sqrt(i)) == Math.sqrt(i)) {
-                            l = (int)(Math.sqrt(i));
+                            perpendicular = (int)(Math.sqrt(i));
                         } else {
-                            l = i;
+                            perpendicular = i;
                         }
                         if (Math.round(Math.sqrt(j)) == Math.sqrt(j)) {
-                            y = (int)(Math.sqrt(j));
+                            hypotenuse = (int)(Math.sqrt(j));
                         } else {
-                            y = j;
+                            hypotenuse = j;
                         }
-                        z = (l+y)+0;
-                        zd.add(i);
-                        zd.add(j);
-                        e.add(z);
-                        h.add(zd);
+                        //Calculating and storing sum of perpendicular and hypotenuse
+                        sum = (perpendicular+hypotenuse)+0;
+                        perpendicular_and_hypotenuse.add(i);
+                        perpendicular_and_hypotenuse.add(j);
+                        all_sums.add(sum);
+                        all_perpendicular_and_hypotenuse.add(perpendicular_and_hypotenuse);
                     }
                 }
             }
         }
-        za = e.get(0);
-        for (int i=0;i<h.size();i++) {
-            za = Math.min(za, e.get(i));
+        //Finding the recommended values of hypotenuse and perpendicular
+        smart_sum = all_sums.get(0);
+        for (int i=0;i<all_perpendicular_and_hypotenuse.size();i++) {
+            smart_sum = Math.min(smart_sum, all_sums.get(i));
         }
-        for(int i=0;i<h.size();i++) {
-            if (za == e.get(i)) {
-                if (h.get(i).get(1) == 0) {
-                    if ((int)(Math.sqrt(h.get(i).get(0))) == Math.sqrt(h.get(i).get(0))) {
-                        aeh.add(h.get(i).get(0));
-                        aeh.add((int)0);
+        //Storing the recommended values to return it
+        for(int i=0;i<all_perpendicular_and_hypotenuse.size();i++) {
+            if (smart_sum == all_sums.get(i)) {
+                if (all_perpendicular_and_hypotenuse.get(i).get(1) == 0) {
+                    if ((int)(Math.sqrt(all_perpendicular_and_hypotenuse.get(i).get(0))) == Math.sqrt(all_perpendicular_and_hypotenuse.get(i).get(0))) {
+                        returner.add(all_perpendicular_and_hypotenuse.get(i).get(0));
+                        returner.add((int)0);
                     }
                 } else {
-                    aeh.add(h.get(i).get(0));
-                    aeh.add(h.get(i).get(1));
+                    returner.add(all_perpendicular_and_hypotenuse.get(i).get(0));
+                    returner.add(all_perpendicular_and_hypotenuse.get(i).get(1));
                 }
             }
         }
-       return aeh;
+        //Returning the hypotenuse and perpendicular
+       return returner;
     }
 }
